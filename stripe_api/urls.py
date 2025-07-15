47fd545391aa7b75f4a0d7b2ea9ee5a3c40ea36d
Ernest Thuku Macharia <ernest.bposeats@gmail.com>
2025-07-15 15:33:00 +0300

    update payments workflow

--- stripe_api/urls.py
+++ stripe_api/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+# from django.urls import path
 
-from . import views
+# from . import views
 
-urlpatterns = [
-    path("create/", views.create_stripe_session, name="stripe-create-session"),
-    path("webhook/", views.stripe_webhook, name="stripe-webhook"),
-]
+# urlpatterns = [
+#     path("create/", views.create_stripe_session, name="stripe-create-session"),
+#     path("webhook/", views.stripe_webhook, name="stripe-webhook"),
+# ]
